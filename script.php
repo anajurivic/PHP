0a90ced131038adcbfc7b9994a2140f602e24123
AnaJulia <ana.vicente43@etec.sp.gov.br>
2024-09-23 22:32:15 -0300

    feito

---
+++ script.php
@@ -0,0 +1,38 @@
+<?php
+
+$nome = $_POST['nome'];
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+$nota4 = $_POST['nota4'];
+
+$media = ($nota1+$nota2+$nota3+$nota4) / 4;
+
+echo "Meu nome é $nome e minha nota final é $media";
+echo "<br>";
+
+if ($media>=6) {
+    echo "O aluno $nome, está aprovado!";
+}
+elseif($media>=4){
+    echo "Recuperação!";
+}
+else {
+    echo "O aluno $nome, está reprovado!";
+}
+
+
+
+?>,
+
+
+
+
+
+
+
+
+
+
+
+
